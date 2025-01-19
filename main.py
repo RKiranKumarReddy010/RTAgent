38b04da112e9d9494d158523737f19293b98820f
KIRAN <ki2003167@gmail.com>
2025-01-19 21:39:27 +0530

    new 19-01-2025

--- main.py
+++ main.py
@@ -9,12 +9,37 @@ from rich.json import JSON
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
 from phi.storage.agent.sqlite import SqlAgentStorage
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+from phi.embedder.ollama import OllamaEmbedder
+from phi.knowledge.csv import CSVUrlKnowledgeBase
+from phi.vectordb.lancedb import LanceDb, SearchType
 
+# Create a knowledge base from a PDF
+knowledge_base = CSVUrlKnowledgeBase(
+    urls=["https://phi-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
+    # Use LanceDB as the vector database
+    vector_db=LanceDb(
+        table_name="recipes",
+        uri="tmp/lancedb",
+        search_type=SearchType.vector,
+        embedder=OllamaEmbedder(model="nomic-embed-text:latest"),
+    ),
+)
+knowledge_base.load(recreate=False)
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4o"),
+    # Add the knowledge base to the agent
+    knowledge=knowledge_base,
+    show_tool_calls=True,
+    markdown=True,
+)
 
 Shell_agent = Agent(
     name="Shell Agent",
-    #model=Ollama(id="mistral-nemo:latest"),
-    model=Groq(id="llama-3.3-70b-versatile"),
+    model=Ollama(id="mistral-nemo:latest"),
+    #model=Groq(id="llama-3.3-70b-versatile"),
     tools=[ShellTools()],
     instructions=["You are responsible for executing the shell commands for windows os. "],
     show_tool_calls=True,
@@ -22,8 +47,8 @@ Shell_agent = Agent(
 )
 
 Memory_agent = Agent(
-    #model=Ollama(id="mistral-nemo:latest"),
-    model=Groq(id="llama-3.3-70b-versatile"),
+    model=Ollama(id="mistral-nemo:latest"),
+    #model=Groq(id="llama-3.3-70b-versatile"),
     storage=SqlAgentStorage(table_name="agent_sessions", db_file="tmp/agent_storage.db"),
     add_history_to_messages=True,
     description="You need to store the response locally. When i run a instruction always refer to previous results and instruction. Based on that run this for current state.",
@@ -34,8 +59,8 @@ console = Console()
 Multi_agent = Agent(
     team=[Shell_agent],
     instructions=  ["always include the source.Also store the previous responses as memory."],
-    #model=Ollama(id="mistral-nemo:latest"),
-    model=Groq(id="llama-3.3-70b-versatile"),
+    model=Ollama(id="mistral-nemo:latest"),
+    #model=Groq(id="llama-3.3-70b-versatile"),
     show_tool_calls=True,
     markdown=True,
 )
@@ -57,3 +82,6 @@ def print_chat_history(agent):
 while(True):
     query = input("Enter Your Query:")
     Multi_agent.print_response(query)
+    if str(input("Are u want to close:(y/n)")) in  ["y","Y"]:
+        break
+    
